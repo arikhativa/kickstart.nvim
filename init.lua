554188f8cff0f92cf469fd3d3a6f32fbbb40bd19
arikhativa <arikhativa@gmail.com>
2025-03-15 17:59:20 +0200

    first changes

--- init.lua
+++ init.lua
@@ -106,6 +106,7 @@ vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
+vim.opt.relativenumber = true
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -180,10 +181,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -193,6 +194,52 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: custom mappings
+
+local term_buf = -1
+local term_win = -1
+local last_win = -1
+
+function ToggleTerminal()
+  local current_win = vim.api.nvim_get_current_win()
+
+  if term_win ~= -1 and vim.api.nvim_win_is_valid(term_win) then
+    -- If terminal window is open, close it and return to the last window
+    vim.api.nvim_win_hide(term_win)
+    term_win = -1
+    vim.api.nvim_set_current_win(last_win)
+  else
+    -- Save the last window before opening the terminal
+    last_win = current_win
+
+    if term_buf == -1 or not vim.api.nvim_buf_is_valid(term_buf) then
+      -- Create a new terminal buffer if none exists
+      vim.cmd 'split'
+      vim.cmd 'term'
+      term_buf = vim.api.nvim_get_current_buf()
+    else
+      -- Open terminal in the same position
+      vim.cmd 'split'
+      vim.api.nvim_set_current_buf(term_buf)
+    end
+
+    -- Store the new terminal window ID
+    term_win = vim.api.nvim_get_current_win()
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<leader>st', ':lua ToggleTerminal()<CR>', { noremap = true, silent = true })
+
+--vim.keymap.set('n', '<leader>st', function()
+-- vim.cmd.vnew()
+--vim.cmd.term()
+--vim.cmd.wincmd 'J'
+--vim.api.nvim_win_set_height(0, 10)
+--end)
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and keep curser at center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move down and keep curser at center' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -205,6 +252,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
   end,
 })
 
@@ -663,6 +718,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -911,6 +967,9 @@ require('lazy').setup({
     end,
   },
 
+  -- NOTE: custom plugins
+  --  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -1027,3 +1086,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+require 'lsp'
