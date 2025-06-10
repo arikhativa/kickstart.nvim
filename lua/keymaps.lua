b179df936798bf7b14e2184cf4b9fcacb3889c8f
Arik Hativa <arikhativa@gmail.com>
2025-06-10 11:41:04 +0300

    fix: move keynids

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,8 +34,8 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down and keep curser at c
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up and keep curser at center' })
 
 -- NOTE: move cmd
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
 
